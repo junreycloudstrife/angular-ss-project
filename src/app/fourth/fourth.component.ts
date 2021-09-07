b962bd0215153588c4ba1c2f46fd32c663dca5e7
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-09-07 13:40:08 +0800

    created the third & fourth component

---
+++ src/app/fourth/fourth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-fourth',
+    template: `<p>fourth works!</p>`,
+    styles: [ 'p { color: blue }' ]
+})
+
+export class FourthComponent{
+
+}
