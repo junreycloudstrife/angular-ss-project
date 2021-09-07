cc5118b41108a9855bba5c605af9d15e6c9e1cec
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-09-07 12:32:18 +0800

    components

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-second',
+    template: `<p>second works!</p>`,
+    styles: ['p { color: red }']
+})
+
+export class SecondComponent {
+
+}
