5e3d8ba980905b6e90b9a8fbf2d4d03c9f564828
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-09-23 16:39:11 +0800

    input and output parameter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ss-project';
+
+  appMessage = "Message from AppComponent to FirstComponent";
+
+  appMethod(){
+    alert('Click Me! button works');
+  }
 }
